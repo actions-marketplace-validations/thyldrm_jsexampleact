d4c9be1e01669111cfd39c5651e030007a8647b6
Taha Yıldırım <thyldrm23@gmail.com>
2022-12-05 15:04:27 +0300

    act act

--- index.js
+++ index.js
@@ -2,23 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const token = core.getInput('ACCESS_TOKEN');
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-  console.log(github);
-  console.log("----------------------")
-  console.log("----------------------")
-  console.log("----------------------")
-  console.log(nameToGreet);
-  console.log("----------------------")
-  console.log("----------------------")
-  console.log("----------------------")
+
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+ 
   console.log(token);
 
   const token1 = process.env.ACCESS_TOKEN;
