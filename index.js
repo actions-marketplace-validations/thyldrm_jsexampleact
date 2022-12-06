e489943c069c592b9ee1f418239339fb4df749bd
Taha Yıldırım <thyldrm23@gmail.com>
2022-12-06 17:22:29 +0300

    act act

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require("axios");
 
 try {
   const token = core.getInput('ACCESS_TOKEN');
+
   
 
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
@@ -12,17 +13,21 @@ try {
   console.log(token);
 
   const token1 = process.env.ACCESS_TOKEN;
+  const USERNAME = process.env.USERNAME;
+  const PASSWORD = process.env.PASSWORD;
   console.log("----------------------")
   console.log("----------------------")
   console.log("----------------------")
   console.log(token1);
+  const b64uidpss = btoa(`${USERNAME}:${PASSWORD}`);
+  const authorization = `Basic ${b64uidpss}`;
   axios
-  .get(`http://${LOCALHOST_IP}:3000/api/users`, {
+  .post(`https://dev.codethreat.com/signin`, {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json;charset=UTF-8",
       "x-ct-organization": "codethreat",
-      "Authorization": `${token1}`
+      "Authorization": `${authorization}`
     },
   })
   .then(({data}) => {
