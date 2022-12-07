3acb1f8c97bca6223f8787518d51d735c6e4f4b9
Taha Yıldırım <thyldrm23@gmail.com>
2022-12-07 11:09:09 +0300

    act act

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ try {
 // });
 const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  console.log("eyyy " + github.context.repo.name);
+  console.log(`NAMEEE: ${payload.name}`);
+  console.log("eyyy " + github.context.payload.name);
 } catch (error) {
   core.setFailed(error.message);
 }
