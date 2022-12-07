a193947b7bf38430e2b8bbb00619a31844d436aa
Taha Yıldırım <thyldrm23@gmail.com>
2022-12-07 11:39:54 +0300

    act act

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ const axios = require("axios");
 try {
 //   const token1 = process.env.ACCESS_TOKEN;
 //   const options = {
+//     headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json;charset=UTF-8",
+//         "x-ct-organization": "codethreat",
+//         "Authorization": `${token1}`
+//       },
 //     method: 'post',
 //     url: 'https://501e-78-189-89-238.eu.ngrok.io/integration/github/start',
 //     data: {
-//       project: 'Finn',
-//       lastName: 'Williams'
+//       project: github.context.payload.repository.name,
 //     },
-//     transformRequest: [(data, headers) => {
-//       // transform the data
-  
-//       return data;
-//     }]
 //   };
+//   axios(options).then(({data}) => {
+//     console.log(data);
+// });
+
+
 //   axios
 //   .post("https://501e-78-189-89-238.eu.ngrok.io/integration/github/start", {
 //     headers: {
@@ -29,9 +34,9 @@ try {
 //   .then(({data}) => {
 //     console.log(data);
 // });
-const payload = JSON.stringify(github.context.payload.repository.name)
+
+const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-//   console.log(`NAMEEE: ${payload.repository.name}`);
  console.log("eyyy " + github.context.payload.repository.name);
 } catch (error) {
   core.setFailed(error.message);
