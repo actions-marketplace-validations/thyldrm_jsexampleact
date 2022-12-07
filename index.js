d848656596f9a4c5ef6f025b776afc369339086e
Taha Yıldırım <thyldrm23@gmail.com>
2022-12-07 13:58:53 +0300

    act act

--- index.js
+++ index.js
@@ -4,38 +4,43 @@ const axios = require("axios");
 
 try {
   const token1 = process.env.ACCESS_TOKEN;
-  const options = {
-    headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json;charset=UTF-8",
-        "x-ct-organization": "codethreat",
-        "Authorization": `${token1}`
-      },
-    method: 'post',
-    url: 'https://501e-78-189-89-238.eu.ngrok.io/api/integration/github/start',
-    data: {
-      project: github.context.payload.repository.name,
-      branch: github.context.payload.pull_request.head.ref,
-      type: "action"
-    },
-  };
-  axios(options).then(({data}) => {
-    console.log(data);
-});
-
-
-//   axios
-//   .post("https://501e-78-189-89-238.eu.ngrok.io/integration/github/start", {
+//   const options = {
 //     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json;charset=UTF-8",
-//       "x-ct-organization": "codethreat",
-//       "Authorization": `${token1}`
+//         Accept: "application/json",
+//         "Content-Type": "application/json;charset=UTF-8",
+//         "x-ct-organization": "codethreat",
+//         "Authorization": `${token1}`
+//       },
+//     method: 'post',
+//     url: 'https://501e-78-189-89-238.eu.ngrok.io/api/integration/github/start',
+//     data: {
+//       project: github.context.payload.repository.name,
+//       branch: github.context.payload.pull_request.head.ref,
+//       type: "action"
 //     },
-//   })
-//   .then(({data}) => {
+//   };
+//   axios(options).then(({data}) => {
 //     console.log(data);
 // });
+
+
+  axios
+  .post("https://501e-78-189-89-238.eu.ngrok.io/integration/github/start", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+      "x-ct-organization": "codethreat",
+      "Authorization": `${token1}`
+    },
+    body: {
+        project: github.context.payload.repository.name,
+        branch: github.context.payload.pull_request.head.ref,
+        type: "action"
+      },
+  })
+  .then(({data}) => {
+    console.log(data);
+});
 
 const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
